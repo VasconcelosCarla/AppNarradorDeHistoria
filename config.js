8deaac6e852ce340dd49b96d1e9d313cedbf800d
Carla Vasconcelos <88123492+VasconcelosCarla@users.noreply.github.com>
2023-03-02 00:24:30 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig ={
+    
+    apiKey: "AIzaSyC9DroGw6AMiPwkZ4LW7d17FFbJjIq3TcM",
+    authDomain: "appnarradordehistoria.firebaseapp.com",
+    databaseURL: "https://appnarradordehistoria-default-rtdb.firebaseio.com",
+    projectId: "appnarradordehistoria",
+    storageBucket: "appnarradordehistoria.appspot.com",
+    messagingSenderId: "306008682878",
+    appId: "1:306008682878:web:07257951a16a1d3081032f"
+      
+}
---
+++ navegation/TabNavigator.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Feed from "../screens/Feed";
+import CreateStory from "../screens/createStory";
+
+const Tab = createBottomTabNavigator();
+
